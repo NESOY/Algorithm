4f15661ef90e14f8c19241727811148dd85e4c63
Kwon Young Jae <kyoje11@gmail.com>
2019-01-04 22:07:31 +0900

    Update Solution using window

--- src/Leetcode/longest_substring_without_repeating_characters.java
+++ src/Leetcode/longest_substring_without_repeating_characters.java
@@ -8,18 +8,17 @@ import java.util.Map;
  */
 public class longest_substring_without_repeating_characters {
 	public int lengthOfLongestSubstring(String s) {
-		char[] array = s.toCharArray();
 		int longestLength = 0;
 
-		for(int i=0; i<array.length; i++){
+		for(int i=0; i<s.length(); i++){
 			Map<Character, Boolean> findMap = new HashMap<>();
 			int length = 0;
-			for(int j = i; j<array.length; j++) {
-				char key = array[j];
+			for(int j = i; j<s.length(); j++) {
+				char key = s.charAt(i);
 				if (findMap.containsKey(key)) {
 					break;
 				}
-				findMap.put(key, true);
+				findMap.put(key, true); // Improve Point : Save Past Index
 				length++;
 			}
 
@@ -28,5 +27,20 @@ public class longest_substring_without_repeating_characters {
 
 
 		return longestLength;
+	}
+
+
+	public int lengthOfLongestSubstringTOWindow(String s) {
+		int n = s.length(), ans = 0;
+		Map<Character, Integer> map = new HashMap<>(); // current index of character
+		// try to extend the range [i, j]
+		for (int j = 0, i = 0; j < n; j++) {
+			if (map.containsKey(s.charAt(j))) {
+				i = Math.max(map.get(s.charAt(j)), i);
+			}
+			ans = Math.max(ans, j - i + 1);
+			map.put(s.charAt(j), j + 1);
+		}
+		return ans;
 	}
 }
