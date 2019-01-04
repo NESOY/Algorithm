02548450555e867452809543507599110eb3bdf6
YoungJae Kwon <kyoje11@gmail.com>
2019-01-04 23:59:09 +0900

    Update Two numbers

--- test/Leetcode/test_two_sum.java
+++ test/Leetcode/test_two_sum.java
@@ -1,30 +1,30 @@
 package Leetcode;
 
-import java.util.Arrays;
-
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertTrue;
 
 public class test_two_sum {
 
-	@Test
-	public void target에_해당하는_Index배열을_얻을수있다(){
-		two_sum solution = new two_sum();
-		int[] nums = {2, 7, 9,15};
-		int[] ans = {0, 1};
+    @Test
+    public void target에_해당하는_Index배열을_얻을수있다() {
+        two_sum solution = new two_sum();
+        int[] nums = {2, 7, 9, 15};
+        int[] ans = {0, 1};
 
-		System.out.println(Arrays.toString(solution.twoSum(nums, 9)));
-		assertTrue(Arrays.equals(ans, solution.twoSum(nums, 9)));
-	}
+        System.out.println(Arrays.toString(solution.twoSum(nums, 9)));
+        assertTrue(Arrays.equals(ans, solution.twoSum(nums, 9)));
+    }
 
-	@Test
-	public void 중복되는_경우_target에_해당하는_첫번째_Index배열을_얻을수있다(){
-		two_sum solution = new two_sum();
-		int[] nums = {0, 9 , 2, 7, 0, 9};
-		int[] ans = {0, 1};
+    @Test
+    public void 중복되는_경우_target에_해당하는_첫번째_Index배열을_얻을수있다() {
+        two_sum solution = new two_sum();
+        int[] nums = {0, 9, 2, 7, 0, 9};
+        int[] ans = {0, 1};
 
-		System.out.println(Arrays.toString(solution.twoSum(nums, 9)));
-		assertTrue(Arrays.equals(ans, solution.twoSum(nums, 9)));
-	}
+        System.out.println(Arrays.toString(solution.twoSum(nums, 9)));
+        assertTrue(Arrays.equals(ans, solution.twoSum(nums, 9)));
+    }
 }
